80f9b38a7fa3cc37d01368aaf0c1c2c9223f2a78
Saman Hazemi <s.hazemi.jebelli@gmail.com>
2024-01-28 00:29:36 +0330

    feat: normalizer skeleton

--- tools/normalizer.py
+++ tools/normalizer.py
@@ -1,64 +1,83 @@
 import re
 from hazm import Normalizer
 
-# These should be right before a word seperated by a half space.
-SPACING_PATTERN = ['ی', 'ای', 'ها', 'های', 'تر', 'تری', 'ترین', 'گر', 'گری', 'ام', 'ات', 'اش', 'اعداد', 'می', 'نمی']
-
-# The english numbers within the text should be converted into persian numbers.
-NUMBERS = {
-    ord('0'): '۰',
-    ord('1'): '۱',
-    ord('2'): '۲',
-    ord('3'): '۳',
-    ord('4'): '۴',
-    ord('5'): '۵',
-    ord('6'): '۶',
-    ord('7'): '۷',
-    ord('8'): '۸',
-    ord('9'): '۹',
-}
-
-# FATHATAN, DAMMATAN, KASRATAN, FATHA, DAMMA, KASRA, SHADDA, SUKUN
-
-STOP_WORDS = []
-
-SPECIAL_WORDS = {
-    "﷽": "بسم الله الرحمن الرحیم",
-    "﷼": "ریال",
-    "(ﷰ|ﷹ)": "صلی",
-    "ﷲ": "الله",
-    "ﷳ": "اکبر",
-    "ﷴ": "محمد",
-    "ﷵ": "صلعم",
-    "ﷶ": "رسول",
-    "ﷷ": "علیه",
-    "ﷸ": "وسلم",
-    "ﻵ|ﻶ|ﻷ|ﻸ|ﻹ|ﻺ|ﻻ|ﻼ": "لا"
-}
-
 
 class Normalizer:
-    _spacing = True
-    _numbers = True
-    _stop = True
-    _special = True
-    _unnecessary = True
+    # These should be right before a word seperated by a half space.
+    SPACING_PATTERN = ['ی', 'ای', 'ها', 'های', 'تر', 'تری', 'ترین', 'گر', 'گری', 'ام', 'ات', 'اش', 'اعداد', 'می', 'نمی']
 
-    def __init__(self, spacing=False, numbers=False, patterns=[]) -> None:
-        self._spacing = spacing
-        self._numbers = numbers
-        self.patterns = patterns
+    # The english numbers within the text should be converted into persian numbers.
+    NUMBERS = {
+        ord('0'): '۰',
+        ord('1'): '۱',
+        ord('2'): '۲',
+        ord('3'): '۳',
+        ord('4'): '۴',
+        ord('5'): '۵',
+        ord('6'): '۶',
+        ord('7'): '۷',
+        ord('8'): '۸',
+        ord('9'): '۹',
+    }
+
+    SPECIAL_WORDS = {
+        "﷽": "بسم الله الرحمن الرحیم",
+        "﷼": "ریال",
+        "(ﷰ|ﷹ)": "صلی",
+        "ﷲ": "الله",
+        "ﷳ": "اکبر",
+        "ﷴ": "محمد",
+        "ﷵ": "صلعم",
+        "ﷶ": "رسول",
+        "ﷷ": "علیه",
+        "ﷸ": "وسلم",
+        "ﻵ|ﻶ|ﻷ|ﻸ|ﻹ|ﻺ|ﻻ|ﻼ": "لا"
+    }
+
+    def __init__(
+            self,
+            correct_spacing: bool = True,
+            unicodes_replacement: bool = True,
+            remove_diacritics: bool = True,
+            remove_specials_chars: bool = True,
+            persian_numbers: bool = True
+    ) -> None:
+        self._correct_spacing = correct_spacing
+        self._unicodes_replacement = unicodes_replacement
+        self._remove_diacritics = remove_diacritics
+        self._remove_specials_chars = remove_specials_chars
+        self._persian_numbers = persian_numbers
 
     def normalize(self, text: str) -> str:
-        if self._numbers:
-            text = self.normalize_numbers(text)
+        if self._correct_spacing:
+            text = Normalizer._correct_spacing(text=text)
+        if self._unicodes_replacement:
+            text = Normalizer._unicodes_replacement(text=text)
+        if self._remove_diacritics:
+            text = Normalizer._remove_diacritics(text=text)
+        if self._remove_specials_chars:
+            text = Normalizer._remove_specials_chars(text=text)
+        if self._persian_numbers:
+            text = Normalizer._persian_numbers(text=text)
         return text
 
     @staticmethod
-    def normalize_numbers(text: str) -> str:
-        return text.translate(NUMBERS)
+    def _correct_spacing(text: str) -> str:
+        ...
 
     @staticmethod
-    def special_words(text: str) -> str:
-        for special in SPECIAL_WORDS:
-            return re.sub(r'special', '', text)
+    def _unicodes_replacement(text: str) -> str:
+        ...
+
+    @staticmethod
+    def _remove_diacritics(text: str) -> str:
+        ...
+
+    @staticmethod
+    def _remove_specials_chars(text: str) -> str:
+        ...
+
+    @staticmethod
+    def _persian_numbers(text: str) -> str:
+        ...
+
